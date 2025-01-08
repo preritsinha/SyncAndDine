26758e6ec2ae66ccbedd1850ec3fc0cc7ab91273
Prerit Sinha <preritsinha1997@gmail.com>
2025-01-09 01:19:37 +0530

    update app.py

--- app.py
+++ app.py
@@ -281,4 +281,4 @@ if __name__ == '__main__':
     if not os.path.exists('/instance/syncanddine.db'):
         with app.app_context():
             db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 10000)))
